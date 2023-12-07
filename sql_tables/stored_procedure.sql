32280367bb13dee7d3a59ee8348832d409455beb
Priyansh Tratiya <77935498+PriyanshTratiya@users.noreply.github.com>
2023-12-06 18:02:28 -0600

    Update stored_procedure.sql

--- sql_tables/stored_procedure.sql
+++ sql_tables/stored_procedure.sql
@@ -6,7 +6,6 @@ DROP PROCEDURE IF EXISTS GetChannelStatistics;
 DELIMITER //
 CREATE PROCEDURE `GetChannelStatistics`()
 BEGIN
-    -- Temporary variables to store averages
     DECLARE `temp_avgViews` REAL;
     DECLARE `temp_avgLikes` REAL;
     DECLARE `temp_avgComments` REAL;
@@ -15,8 +14,8 @@ BEGIN
     DECLARE `done` INT DEFAULT 0;
     DECLARE `temp_channel_id` VARCHAR(255);
     DECLARE `temp_channel_title` VARCHAR(255);
+    DECLARE `Current_Popularity_Level` INT;
 
-    -- Cursor to iterate through each channel
     DECLARE `channelCursor` CURSOR FOR
         SELECT `c`.`channel_id`, `c`.`channel_title`
         FROM `channel` `c`
@@ -24,8 +23,6 @@ BEGIN
         GROUP BY `c`.`channel_id`;
 
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET `done` = 1;
-   
-    -- Check if the ChannelStatistics table exists; if not, create it
     CREATE TABLE IF NOT EXISTS `ChannelStatistics` (
         `channel_id` VARCHAR(255),
         `channel_title` VARCHAR(255),
@@ -34,6 +31,7 @@ BEGIN
         `avgComments` REAL,
         `total_videos` INT,
         `max_view_count` BIGINT,
+        `popularity_level` INT,
         PRIMARY KEY (`channel_id`)
     );
 
@@ -41,38 +39,43 @@ BEGIN
     REPEAT
         FETCH `channelCursor` INTO `temp_channel_id`, `temp_channel_title`;
 
-        -- Advanced Query 1: Calculate averages for each channel
         SELECT AVG(`vs`.`view_count`), AVG(`vs`.`likes`), AVG(`vs`.`comment_count`)
         INTO `temp_avgViews`, `temp_avgLikes`, `temp_avgComments`
         FROM `video_stats` `vs`
         INNER JOIN `video` `v` ON `vs`.`video_id` = `v`.`video_id`
         WHERE `v`.`channel_id` = `temp_channel_id`;
 
-        -- Advanced Query 2: Calculate total number of videos for each channel
         SELECT COUNT(*)
         INTO `temp_total_videos`
         FROM `video`
         WHERE `channel_id` = `temp_channel_id`;
 
-        -- Advanced Query 3: Find maximum view count for videos in each channel
         SELECT MAX(`vs`.`view_count`)
         INTO `temp_max_view_count`
         FROM `video_stats` `vs`
         INNER JOIN `video` `v` ON `vs`.`video_id` = `v`.`video_id`
         WHERE `v`.`channel_id` = `temp_channel_id`;
 
-        -- Insert or update the ChannelStatistics table
-        INSERT INTO `ChannelStatistics`(`channel_id`, `channel_title`, `avgViews`, `avgLikes`, `avgComments`, `total_videos`, `max_view_count`)
-        VALUES(`temp_channel_id`, `temp_channel_title`, `temp_avgViews`, `temp_avgLikes`, `temp_avgComments`, `temp_total_videos`, `temp_max_view_count`)
+        IF `temp_avgViews` > 0 AND `temp_avgViews` < 1000000 THEN  
+            SET `Current_Popularity_Level` = 1;
+        ELSEIF `temp_avgViews` >= 1000000 AND `temp_avgViews` < 3000000 THEN
+            SET `Current_Popularity_Level` = 2;
+        ELSE    
+            SET `Current_Popularity_Level` = 3;
+        END IF;
+
+        INSERT INTO `ChannelStatistics`(`channel_id`, `channel_title`, `avgViews`, `avgLikes`, `avgComments`, `total_videos`, `max_view_count`, `popularity_level`)
+        VALUES(`temp_channel_id`, `temp_channel_title`, `temp_avgViews`, `temp_avgLikes`, `temp_avgComments`, `temp_total_videos`, `temp_max_view_count`, `Current_Popularity_Level`)
         ON DUPLICATE KEY UPDATE
             `avgViews` = `temp_avgViews`,
             `avgLikes` = `temp_avgLikes`,
             `avgComments` = `temp_avgComments`,
             `total_videos` = `temp_total_videos`,
-            `max_view_count` = `temp_max_view_count`;
+            `max_view_count` = `temp_max_view_count`,
+            `popularity_level` = `Current_Popularity_Level`;
 
     UNTIL `done` END REPEAT;
     CLOSE `channelCursor`;
-END //
+END//
 
-DELIMITER;
+DELIMITER ;
